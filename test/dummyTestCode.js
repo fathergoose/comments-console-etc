0d7b7b046af56dfd4e28c17cbfe5607ee0acc996
Al Ilseman <al@ilseman.com>
2022-08-14 13:24:44 -0500

    Little js file for testing

---
+++ test/dummyTestCode.js
@@ -0,0 +1,20 @@
+// I actually care quit a lot
+var whoCares = 'not me!';
+
+/**
+ * Jsdoc style comment do describe a public function
+ *
+ * @remarsk This is totally fake code
+ *
+ */
+
+export const dummyFunction = () => {
+  // TODO: Change to "wassup"
+  // Multi-line single line comment
+  /* if (process.env['ENVIRONMENT']) {
+  /*   callFunction()
+  /* } else {
+  /*   return 0
+  */
+  console.log(whoCares, 'helloooooo');
+};
